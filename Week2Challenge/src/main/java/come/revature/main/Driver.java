061bf592ca8922d116d9a92e63e29a5d101f06f4
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-09-16 13:41:46 -0400

    created two prepared statements that print info to eclipse

--- Week2Challenge/src/main/java/come/revature/main/Driver.java
+++ Week2Challenge/src/main/java/come/revature/main/Driver.java
@@ -1,6 +1,8 @@
 package come.revature.main;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import come.revature.util.ConnectionUtil;
 
@@ -12,6 +14,35 @@ public class Driver {
 			String filename = "src/main/resources/connection.properties";
 			Connection conn = ConnectionUtil.getConnectionFromFile(filename);
 			System.out.println(conn);
+			
+			// prepared statement for coding challenge week 2
+			String deptQuery = "SELECT * FROM DEPARTMENT";
+			PreparedStatement ps = conn.prepareStatement(deptQuery);
+			boolean isExecuted = ps.execute(); // returns true if it worked
+			System.out.println("Prepared statement for dept executed: " + isExecuted);
+			ResultSet rs = ps.getResultSet();
+			String departmentName = "";
+			while(rs.next()) {
+				departmentName = rs.getString("DEPARTMENT_NAME");
+				System.out.println(departmentName);
+			}
+			
+			// prepared statement for coding challenge week 2
+			String emplQuery = "SELECT * FROM EMPLOYEE";
+			ps = conn.prepareStatement(emplQuery);
+			isExecuted = ps.execute(); // returns true if it worked
+			System.out.println("Prepared statement for empl executed: " + isExecuted);
+			rs = ps.getResultSet();
+			String empFirstname = "";
+			String empSalary = "";
+			String empDeptID = "";
+			while(rs.next()) {
+				empFirstname = rs.getString("EMP_FIRSTNAME");
+				empSalary = rs.getString("SALARY");
+				empDeptID = rs.getString("DEPARTMENT_ID");
+				System.out.println(empFirstname + " : " + empSalary + " : " + empDeptID);
+			}
+			
 		}
 		catch (Exception e) {
 			e.printStackTrace();
