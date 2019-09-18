67f1e3e21aedaae983a7aed9e64f7417b5891496
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-09-18 10:57:42 -0400

    fixed up the html demo group project

--- JDBCBank/src/main/java/com/revature/main/Driver.java
+++ JDBCBank/src/main/java/com/revature/main/Driver.java
@@ -6,22 +6,28 @@ import com.revature.user.User;
 
 public class Driver {
 
+	private static Scanner scanner = new Scanner(System.in);
+	private static String input = null;
+	private static boolean isON = true;
+
 	public static void main(String[] args) {
 
-		System.out.println("Would you like to |CREATE| , |LOGIN| , or |EXIT| ?");
-		Scanner scanner = new Scanner(System.in);
-		String input = null;
-		input = scanner.nextLine();
-		switch(input) {
-		case "CREATE":
-			User.create();
-			break;
-		case "LOGIN":
-			User.login();
-			break;
-		case "EXIT":
-			System.out.println("EXITING-");
-		}
+		do {
+			System.out.println("Welcome to Rex's JDBCBank Console Application!");
+			System.out.println("Would you like to |CREATE| , |LOGIN| , |ADMIN LOGIN| or |EXIT| ?");
+			input = scanner.nextLine();
+			switch (input) {
+			case "CREATE":
+				User.create();
+				break;
+			case "LOGIN":
+				User.login();
+				break;
+			case "EXIT":
+				System.out.println("EXITING-");
+				break;
+			}
+		} while (isON);
 		scanner.close();
 	}
 
--- Notes/RevatureDay11.md
+++ Notes/RevatureDay11.md
@@ -5,3 +5,28 @@
 is modifying layout of pages for different devices
 how things look like on a full are different than
 how they look on a giant tv
+-- plain css w/ media queries
+-- library like bootstrap
+-- flexible grid lines
+
+## bootstrap 4
+
+made by twitter
+libraries on css + javascript which can be included
+in your front_end for stlying and responsiveness
+
+-- can download and link it locally
+-- can use CDN: Content Delivery Network
+
+## bootstrap grid lines
+
+12 columns
+the grid lines allow for a relocation of the elements
+within the screen depending on the viewport size
+
+## group assignment
+
+rebuild yesterday's size with bootstrap
+responsive navbar
+1 other custom component
+different number of columns depending on screen size
