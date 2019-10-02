bc240f6cee23c2a1b7e27b88faeae8c0d81ea4d0
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-01 22:58:52 -0400

    fixed the error page and printed useful error messages to console- server side

--- Reimbursement/src/main/java/com/revature/servlet/ErrorHandlerServlet.java
+++ Reimbursement/src/main/java/com/revature/servlet/ErrorHandlerServlet.java
@@ -13,14 +13,26 @@ public class ErrorHandlerServlet extends HttpServlet {
 
 	// Method to handle GET method request.
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// getting useful information about the error that occurred client side
 		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+		Class<?> exceptionType = (Class<?>) request.getAttribute("javax.servlet.error.exception_type");
+		String message = (String) request.getAttribute("javax.servlet.error.message");
+		String requestUri = (String) request.getAttribute("javax.servlet.error.request_uri");
+		String exception = (String) request.getAttribute("javax.servlet.error.exception");
+		String servletName = (String) request.getAttribute("javax.servlet.error.servlet_name");
+		// displaying it to the console
 		System.out.println("StatusCode: " + statusCode);
-		response.sendRedirect("Error.html");
+		System.out.println("ExceptionType: " + exceptionType);
+		System.out.println("Message: " + message);
+		System.out.println("RequestUri: " + requestUri);
+		System.out.println("Exception: " + exception);
+		System.out.println("ServletName: " + servletName);
+		// redirecting them to the error page
+		request.getRequestDispatcher("Error.html").forward(request, response);
 	}
 
 	// Method to handle POST method request.
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		doGet(request, response);
 	}
 }
